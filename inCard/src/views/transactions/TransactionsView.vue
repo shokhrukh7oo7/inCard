79df71e0b598897d77e87f9604d4b22eb6983679
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2025-11-17 16:30:55 +0500

    transactions page finished with responsive and dark mode

--- inCard/src/views/transactions/TransactionsView.vue
+++ inCard/src/views/transactions/TransactionsView.vue
@@ -1,6 +1,9 @@
 <script setup>
-// import { ref } from 'vue';
+import { ref } from 'vue';
 import BaseFilter from '@/components/BaseFilter.vue';
+import BaseButton from '@/components/BaseButton.vue';
+import BaseTable from '@/components/BaseTable.vue';
+import BaseSelect from '@/components/BaseSelect.vue';
 
 const transactionCompanies = [
     { value: 'filial', label: "Головной офис" },
@@ -31,16 +34,75 @@ const transactionFields = [
     { type: 'select', model: 'company', props: { options: AllGateway, placeholder: 'Все' } },
 ]
 
+// EXPORT SELECT
+const selectedCompany = ref("")
+const companies = [
+    { value: "Все", label: "Все" },
+    { value: "ACCENT LINE OOO (BOSCH)", label: "ACCENT LINE OOO (BOSCH)" },
+    { value: "ADILAB OOO", label: "ADILAB OOO" },
+]
+
+// TABLE
+const transactionsColumns = [
+    { key: 'id', label: '№' },
+    { key: 'idPayment', label: 'ID Платежа' },
+    { key: 'fullName', label: 'ФИО' },
+    { key: 'idContract', label: 'ID контракта' },
+    { key: 'sum', label: 'Сумма' },
+    { key: 'comission', label: 'Комиссия' },
+    { key: 'cardNumber', label: 'Номер карты' },
+    { key: 'extTransactions', label: 'Ext Транзакции' },
+    { key: 'gateway', label: 'Шлюз' },
+    { key: 'date', label: 'Дата' },
+    { key: 'actions', label: 'Перейти' },
+]
+
+const transactionsData = ref([])
+
 const handleSearch = filters => {
     console.log('Фильтры:', filters)
     // тут можно вызывать API или фильтровать локальные данные
 }
+
 </script>
 
 <template>
     <div class="transactions-wrapper">
-        <BaseFilter :fields="transactionFields" title="Фильтры поиска" @search="handleSearch" />
+        <div class="transactions-filter-wrapper">
+            <BaseFilter :fields="transactionFields" title="Фильтры поиска" @search="handleSearch" />
+        </div>
 
+        <div class="transactions-table-wrapper">
+            <div class="transactions-filter-wrapper">
+                <div class="transaction-left-wrapper">
+                    <p>Всего: 0</p>
+                    <p>Общая сумма разностей: 0</p>
+                    <p>Общая сумма: 0</p>
+                    <p>Общая сумма возврата: 0</p>
+                </div>
+
+                <div class="transaction-right-wrapper">
+                    <BaseButton>
+                        <img src="../../assets/images/table/arrow-export.svg" alt="image">
+                        Export
+                    </BaseButton>
+
+                    <BaseSelect v-model="selectedCompany" :options="companies" placeholder="Все" />
+                </div>
+            </div>
+
+            <div class="table-wrapper">
+                <div class="table-component">
+                    <BaseTable :columns="transactionsColumns" :data="transactionsData">
+                        <template #actions="{ row }">
+                            <router-link :to="`/payers/${row.id}`" title="Перейти">
+                                <img src="@/assets/images/table/arrow-circle.svg" alt="image" />
+                            </router-link>
+                        </template>
+                    </BaseTable>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
